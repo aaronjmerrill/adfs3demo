59dde1166eca5fc1427e3fffb2f185f85a0274cc
Aaron Merrill <aaron@merrills.website>
2022-05-02 22:41:42 -0500

    azure read works. Wrapping boto3 in try block to log error

--- function/AzureToS3/__init__.py
+++ function/AzureToS3/__init__.py
@@ -14,20 +14,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         file_path='goesToS3.json'
     )
     logging.info('created az file client')
-
-    # with open("goesToS3.json", "wb") as file_handle:
-    #     data = file_client.download_file()
-    #     data.readinto(file_handle)
+    
     data = file_client.download_file()
     logging.info('read file')
 
-    s3 = boto3.resource(
-        endpointUrl='https://s3.us-west-002.backblazeb2.com',
-        service_name='s3',
-        aws_access_key_id='002fdf2ee4eb9b00000000004',
-        aws_secret_access_key='K002eu2wpB+ig0bBhmpO8D2Wc74U5tY'
-    )
-    logging.info('created S3 resource')
+    try:
+        s3 = boto3.resource(
+            endpointUrl='https://s3.us-west-002.backblazeb2.com',
+            service_name='s3',
+            aws_access_key_id='002fdf2ee4eb9b00000000004',
+            aws_secret_access_key='K002eu2wpB+ig0bBhmpO8D2Wc74U5tY'
+        )
+        logging.info('created S3 resource')
+    except Exception as e: 
+        logging.info(e)
 
     s3.Object('adfs3demoinput').Put(Body=data)
     logging.info('uploaded file to S3')
